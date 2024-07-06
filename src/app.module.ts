f637eed2eb96d247908ba5c752d96a8c9bef482a
bryanvengwa <bmvengwa@gmail.com>
2024-07-07 01:40:00 +0200

    created auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://bmvengwa:rNaEui1MdTeswaHt@nest-starter-cluster.7kpsbfq.mongodb.net/'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://bmvengwa:rNaEui1MdTeswaHt@nest-starter-cluster.7kpsbfq.mongodb.net/'), UsersModule, AuthModule],
 })
 export class AppModule {}
