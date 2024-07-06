82e99712bc672d22743939cdfffabea59be232ca
bryanvengwa <bmvengwa@gmail.com>
2024-07-07 01:18:31 +0200

    created user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://bmvengwa:rNaEui1MdTeswaHt@nest-starter-cluster.7kpsbfq.mongodb.net/')],
+  imports: [MongooseModule.forRoot('mongodb+srv://bmvengwa:rNaEui1MdTeswaHt@nest-starter-cluster.7kpsbfq.mongodb.net/'), UsersModule],
 })
 export class AppModule {}
