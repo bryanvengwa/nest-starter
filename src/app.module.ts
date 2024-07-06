df747c7a7c6fb18dd56c5d9c157d6744321bb67d
bryanvengwa <bmvengwa@gmail.com>
2024-07-07 01:12:06 +0200

    successful dataase connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://bmvengwa:<password>@cluster0.8ftkdqj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')],
+  imports: [MongooseModule.forRoot('mongodb+srv://bmvengwa:rNaEui1MdTeswaHt@nest-starter-cluster.7kpsbfq.mongodb.net/')],
 })
 export class AppModule {}
